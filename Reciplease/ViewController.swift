6a25db42c2de02695b97cec8d5d4ee4910c27693
mthaam <79216305+mthaam@users.noreply.github.com>
2021-12-09 20:41:39 +0100

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by JEAN SEBASTIEN BRUNET on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
